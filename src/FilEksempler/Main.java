8869b2af9d67a570a8958efe049b518af83318da
susannejung <95688233+susannejung@users.noreply.github.com>
2023-04-11 08:47:33 +0200

    Initial commit

---
+++ src/FilEksempler/Main.java
@@ -0,0 +1,10 @@
+package FilEksempler;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        TestFactory tf=new TestFactory();
+        tf.main(args);
+    }
+}
